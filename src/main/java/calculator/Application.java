a73344c4ec9f728df8ac8af79b3de9c2299e9f74
drawje <drawje@naver.com>
2025-02-06 16:38:14 +0900

    style
    erase unused import statement

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,7 +3,6 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
-import java.util.Stack;
 
 public class Application {
     public static void main(String[] args) {
